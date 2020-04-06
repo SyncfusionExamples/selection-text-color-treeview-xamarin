5594b7259fe78884dc4a3afcd30ebd9eadf7bea0
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-06 14:59:57 +0530

    XAMARIN-34259 - Sample added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/Behavior.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.XForms.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    public class Behavior : Behavior<SfTreeView>
+    {
+        SfTreeView TreeView;
+        protected override void OnAttachedTo(SfTreeView treeView)
+        {
+            TreeView = treeView;
+            TreeView.SelectionChanging += TreeView_SelectionChanging;
+            base.OnAttachedTo(treeView);
+        }
+        private void TreeView_SelectionChanging(object sender, Syncfusion.XForms.TreeView.ItemSelectionChangingEventArgs e)
+        {
+            if (TreeView.SelectionMode == Syncfusion.XForms.TreeView.SelectionMode.Single)
+            {
+                if (e.AddedItems.Count > 0)
+                {
+                    var item = e.AddedItems[0] as FileManager;
+                    item.LabelColor = Color.Red;
+                }
+                if (e.RemovedItems.Count > 0)
+                {
+                    var item = e.RemovedItems[0] as FileManager;
+                    item.LabelColor = Color.Black;
+                }
+            }
+        }
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            TreeView.SelectionChanging -= TreeView_SelectionChanging;
+			TreeView = null;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
